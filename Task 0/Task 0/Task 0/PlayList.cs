63ebd8ff94afb903998538186e8f064004f9674d
Alex <alexei.truhan@gmail.com>
2020-11-23 14:37:06 +0300

    initial commit

---
+++ Task 0/Task 0/Task 0/PlayList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace Task_0
+{
+    public class PlayList : MediaFile  //дублировать Multimedia
+    {
+        public PlayList(List<MediaFile> mediaFiles) : base(mediaFiles)
+        {
+            MediaFiles = mediaFiles;
+        }
+
+        public PlayList():base()
+        {
+            
+        }
+        public void Play()
+        {
+            Console.WriteLine("Play list:");
+            foreach (var m in MediaFiles)
+            {
+                m.Play();
+            }
+        }
+        
+    }
+}
---
+++ Task 0/Task 0/Task 0/VideoFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Task_0
+{
+    public class VideoFile: MediaFile
+
+    {
+        public int Lenght { get; set; }
+        public VideoFile(string name,int lenght) : base(name)
+        {
+            Name = name;
+            Lenght = lenght;
+        }
+
+        public override void Play()
+        {
+            Console.WriteLine($"Video Play: {Lenght}");
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append($"Name: {Name}\n");
+            stringBuilder.Append($"Lenght: {Lenght}\n");
+            return stringBuilder.ToString();
+        }
+    }
+}
