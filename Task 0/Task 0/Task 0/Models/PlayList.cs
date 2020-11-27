39641193d2089b361dc3977c895985e2d5dfc623
Alex <alexei.truhan@gmail.com>
2020-11-27 16:58:38 +0300

    Add Interface(Imultmedia), rewritte class PlayList and Add fun Play on Multimedia

---
+++ Task 0/Task 0/Task 0/Models/PlayList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace Task_0
+{
+    public class PlayList : MediaFile  //дублировать Multimedia
+    {
+        public List<MediaFile> MediaFiles;
+        
+        public PlayList()
+        {
+            MediaFiles=new List<MediaFile>();
+        }
+        public PlayList(List<MediaFile> mediaFiles)
+        {
+            MediaFiles = new List<MediaFile>();
+        } 
+        
+        public MediaFile this[int index]
+        {
+            get { return MediaFiles[index]; }
+            set { MediaFiles[index] = value; }
+        }
+        
+        public void Add(MediaFile mediaFile)
+        {
+            MediaFiles.Add(mediaFile);
+        }
+        
+        public  void Remove(MediaFile mediaFile)
+        {
+            MediaFiles.Remove(mediaFile);
+        }
+        public void Remove(int index)
+        {
+            MediaFiles.RemoveAt(index);
+        }
+        public int Count()
+        {
+            return MediaFiles.Count;
+        }
+        public override void Play()
+        {
+            Console.WriteLine($"Play list {Name}:");
+            foreach (var m in MediaFiles)
+            {
+                m.Play();
+            }
+        }
+        
+    }
+}
---
+++ Task 0/Task 0/Task 0/Multimeadia/IMultimedia.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace Task_0
+{
+    public interface IMultimedia 
+    {
+
+        public void Add(MediaFile mediaFile)
+        {
+        }
+        
+        public  void Remove(MediaFile mediaFile)
+        {
+        }
+        public void Remove(int index)
+        {
+        }
+    
+    }
+}
